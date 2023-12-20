0374d0b7400ae1a7e146d571f102be5ba00f654c
Robert Heinbokel <robertheinbokel@gmail.com>
2023-12-19 20:56:40 -0500

    Settings and CORS

--- Program.cs
+++ Program.cs
@@ -7,14 +7,36 @@ builder.Services.AddControllers(); //Adds our controllers to our application to 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//CORS Policies
+builder.Services.AddCors((options) =>
+    {
+        options.AddPolicy("DevCors", (corsBuilder) =>
+            {
+                corsBuilder
+                    .WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+        options.AddPolicy("ProdCors", (corsBuilder) =>
+            {
+                corsBuilder.WithOrigins("https://myProductionSite.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+    });
+
 var app = builder.Build(); //app is our built application.
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
 } else {
+    app.UseCors("ProdCors");
     app.UseHttpsRedirection(); // We don't want this while in development locally, generally causes more issues than anything else.
 }
 
